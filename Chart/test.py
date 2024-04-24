2f89e958c5f1614afc64005e14630da06df3cd20
golderalex6 <golderalex6@gmail.com>
2024-04-24 13:41:13 +0700

    BIG CHANGE !! use Sqlite3 to store data on Database folder change from use csv to manipulagte data to use sqlite3 database,folder Chart is use for draw chart,Report folder use for create statistic,financial report,devide job into three separate folder

---
+++ Chart/test.py
@@ -0,0 +1,28 @@
+
+from time import sleep
+import plotext as plt
+import random
+import pandas as pd
+import datetime as dt
+
+df=pd.read_csv('Chart/BTCUSDC-5m-2024-04-01.csv',index_col=0).iloc[:,:4]
+bot,distance=0,50
+df.columns=['Open','High','Low','Close']
+
+def convert(timestamp:int):
+    day=dt.datetime.fromtimestamp(int(timestamp/1000))
+    day_str=dt.datetime.strftime(day,'%Y/%m/%d %H:%M:%S')
+    return day_str
+date=list(map(lambda x:convert(x),df.index))
+
+while True:
+    dat=df.iloc[bot:bot+distance].to_dict(orient='list')
+    day=date[bot:bot+distance]
+    plt.candlestick(day,dat)
+    plt.theme('matrix')
+    plt.show()
+    bot+=1
+    sleep(2)
+    plt.clear_figure()
+
+
