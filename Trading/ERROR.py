a20a75e353b246f37a85514dcf3f808cdff7cced
golderalex6 <golderalex6@gmail.com>
2024-10-26 13:52:41 +0700

    Update credential

--- Trading/ERROR.py
+++ Trading/ERROR.py
@@ -3,13 +3,13 @@ __location__=os.path.dirname(__file__)
 def send_error(err_str):
     url='https://send.api.mailtrap.io/api/send'
     data={
-            'from':{'email':'mailtrap@demomailtrap.com','name':'TranHongLoan'},
-            'to':[{'email':'golderalex6@gmail.com'}],#pass your email here
+            'from':{'email':'mailtrap@demomailtrap.com','name':''},# Your name here
+            'to':[{'email':''}],#pass your email here
             'subject':'Warning!!',
             'text':err_str,
         }
     header={
-            'Authorization': 'Bearer 2950014c5781cfac4e6897364262f3df' ,
+            'Authorization': 'Bearer ' , #Your Bearer
             'Content-Type':'application/json'
             }
     s=requests.post(url,data=json.dumps(data),headers=header)
